6ea67f22c0dbd68e1aeee3a035eefccfab505b32
ramadhan73 <81619894+ramadhan73@users.noreply.github.com>
2021-05-12 18:51:19 +0000

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -28,7 +28,7 @@ async def play(_, message: Message):
                 [
                     InlineKeyboardButton(
                         text="🔊 Channel",
-                        url="https://t.me/Vckyouuu")
+                        url="https://t.me/calonpenyanyi")
                    
                 ]
             ]
